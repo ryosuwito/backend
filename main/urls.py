f9e6ec15a5f583b0bd9db19cf0a681128f395fa5
ynguyen <ynguyen@ynguyen-pc.(none)>
2016-08-29 09:42:15 +0800

    clean html code

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^career$', views.career, name='main.career'),
     url(r'^request$', views.online_test_request, name='main.request'),
     url(r'^culture$', views.culture, name='main.culture'),
+    url(r'^contact$', views.contact, name='main.contact'),
     url(r'^what_we_do$', views.what_we_do, name='main.what_we_do'),
 ]
