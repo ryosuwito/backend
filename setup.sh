ea8c3485af75bddc3852ebe589ca7e6bb1d674a8
ynguyen <ynguyen@dytechlab.com>
2017-02-01 19:04:13 +0800

    add setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
-virtualenv env
-source env/bin/activate
-pip install -r requirements.txt
+set -e
+
+if [ ! -d "env" ]; then
+
+  virtualenv=virtualenv-13.1.2
+  mkdir env
+  cd env
+  wget https://pypi.python.org/packages/source/v/virtualenv/$virtualenv.tar.gz
+  tar xvf $virtualenv.tar.gz
+  cd $virtualenv
+  python virtualenv.py ..
+  cd ../..
+  source env/bin/activate
+  pip install -r requirements.txt
+fi
