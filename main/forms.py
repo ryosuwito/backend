d55b1ec193a6c266b73e49cb5d0524f45216f2da
ynguyen <ynguyen@dytechlab.com>
2017-08-29 15:10:07 +0800

    testreq form validate datetime

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
 from django.utils.translation import ugettext_lazy as _
 from django import forms
 from main.models import OnlineApplication, TestRequest
+from django.utils import timezone
 import datetime
 
 from functools import partial
@@ -32,3 +33,12 @@ class TestRequestForm(forms.ModelForm):
         if self.instance.pk:
             if self.instance.application.position == OnlineApplication.DEVELOPER:
                 del self.fields['version']
+
+    def clean_datetime(self):
+        dt = self.cleaned_data['datetime']
+        if dt < timezone.now():
+            raise forms.ValidationError("The time has already passed")
+        weekno = dt.weekday()
+        if weekno >= 5:
+            raise forms.ValidationError("Please choose weekday (Monday to Friday) only")
+        return dt
