dc13ad2ef45c81504d8d98cb7bb620cba2c6ebf7
ynguyen <ynguyen@dytechlab.com>
2018-03-13 17:24:28 +0800

    send test request 5 min earlier

--- main/cron.py
+++ main/cron.py
@@ -4,6 +4,7 @@ import os
 import traceback
 
 from django.utils import timezone
+from datetime import timedelta
 
 from models import TestRequest
 from emails import send_test
@@ -11,9 +12,10 @@ from emails import send_test
 
 def need_send_test_now(test_request):
     # when testrequest.status=set, and time now pass testrequest.datetime
+    # NOTES: send earlier 5 minutes
     return test_request.status == TestRequest.STATUS_SET \
             and test_request.datetime \
-            and timezone.now() >= test_request.datetime
+            and timezone.now() + timedelta(minutes=5)  >= test_request.datetime
 
 
 def send_online_tests():
@@ -21,6 +23,7 @@ def send_online_tests():
     for req in test_requests:
         try:
             if need_send_test_now(req):
+                # TODO: need to set status to SENDING to avoid email duplicate
                 send_test(req)
                 req.status = TestRequest.STATUS_SENT
                 req.save()
