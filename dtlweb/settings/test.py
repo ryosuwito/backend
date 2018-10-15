4a2d57086f09aeb633f1c552352c42455eef1401
ynguyen <ynguyen@dytechlab.com>
2018-10-15 10:05:54 +0800

    add test env

---
+++ dtlweb/settings/test.py
@@ -0,0 +1,18 @@
+from .const import *
+
+# Overwrite any settings variables of base here
+DEBUG = True
+
+ALLOWED_HOSTS = ['ynguyen-pc.dtl', 'localhost']
+
+COMPANY_CAREER_EMAIL = 'ynguyen@dytechlab.com'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3_test'),
+    }
+}
+
+ADMINS = [('careers-admin', COMPANY_CAREER_EMAIL)]
+SERVER_EMAIL = COMPANY_CAREER_EMAIL
