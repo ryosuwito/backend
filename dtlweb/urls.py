0f151172e6d4944f3a23e7cb28aa58e0d053d591
Do Viet Anh <ado@dytechlab.com>
2021-10-26 10:59:12 +0800

    Disable 2022 recruitment campaign

--- dtlweb/urls.py
+++ dtlweb/urls.py
@@ -18,19 +18,26 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
+from django.shortcuts import redirect
 
 from recruitment_campaign import views
 
+
+def redirect_to_careers(request):
+    return redirect('/careers')
+
+
 urlpatterns = [
     url(r'^dtlweb/admin/', admin.site.urls),
-    url(r'^dtlweb/campaign/', include('recruitment_campaign.urls')),
+    # url(r'^dtlweb/campaign/', include('recruitment_campaign.urls')),  # Oct 26 2021
+    url(r'^dtlweb/campaign/', redirect_to_careers),  # Oct 26 2021
     url(r'^dtlweb/media/campaign/resumes/(?P<file_name>.+)$', views.download_resume, name='campaign.download_resume'),
     url(r'^dtlweb/', include('main.urls')),
-    url(
-        r'^dtlweb/careers/campaign/inviation/(?P<hashstr>[\w:]+)/(?P<action>((accept)|(refuse)))$',
-        views.accept_invitation_to_attend_campaign,
-        name='campaign.career.invitation',
-    ),
+    # url(  # Oct 26 2021
+    #     r'^dtlweb/careers/campaign/inviation/(?P<hashstr>[\w:]+)/(?P<action>((accept)|(refuse)))$',
+    #     views.accept_invitation_to_attend_campaign,
+    #     name='campaign.career.invitation',
+    # ),
 ]
 
 if settings.DEBUG:
